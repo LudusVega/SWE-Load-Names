be103df092b6379524074c7130f9fb017dc8aaa2
jms <jms@SWE112-D>
2024-01-31 17:41:10 -0800

    Add project files.

---
+++ AccessLinkedModelParameters/Class1.cs
@@ -0,0 +1,44 @@
+﻿using Autodesk.Revit.DB;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+public void AccessLinkedModelParameters(Document hostDoc)
+{
+    // Find all Revit link instances in the host model
+    FilteredElementCollector collector = new FilteredElementCollector(hostDoc);
+    var revitLinkInstances = collector.OfCategory(BuiltInCategory.OST_RvtLinks)
+                                      .WhereElementIsNotElementType()
+                                      .Cast<RevitLinkInstance>();
+
+    foreach (var linkInstance in revitLinkInstances)
+    {
+        // Get the Document of the linked model
+        Document linkedDoc = linkInstance.GetLinkDocument();
+
+        // If the linked document is valid, access its elements
+        if (linkedDoc != null)
+        {
+            FilteredElementCollector linkedCollector = new FilteredElementCollector(linkedDoc);
+            var linkedElements = linkedCollector.OfClass(typeof(FamilyInstance)) // or any other class
+                                                 .Where(e => e.Category.Id.IntegerValue == (int)BuiltInCategory.OST_Rooms);
+
+            foreach (var element in linkedElements)
+            {
+                Parameter roomNumberParam = element.get_Parameter(BuiltInParameter.ROOM_NUMBER);
+                if (roomNumberParam != null)
+                {
+                    string roomNumber = roomNumberParam.AsString();
+                    // Process room number as needed
+                }
+            }
+        }
+    }
+}
---
+++ SWE Load Names/SWE Load Names.cs
@@ -0,0 +1,59 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Electrical;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+
+namespace SWE_Load_Names
+{
+    
+
+    [Transaction(TransactionMode.Manual)]
+    [Regeneration(RegenerationOption.Manual)]
+
+    public class ElectricalSystemInfoExtractor : IExternalCommand
+    {
+        private LocationPoint point;
+        private GeometryElement spaceGeometry;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            var electricalSystems = collector.OfClass(typeof(ElectricalSystem));
+            var circuitList = electricalSystems.Cast<ElectricalSystem>().ToList();
+            HashSet<string> spaceNumbers = new HashSet<string>(100);
+            HashSet<string> elementNames = new HashSet<string>(100);
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var circuit in circuitList)
+            {
+                Parameter loadClassParam = circuit.get_Parameter(BuiltInParameter.CIRCUIT_LOAD_CLASSIFICATION_PARAM);
+                
+                var loadClass = loadClassParam.AsString();
+                var elementSet = circuit.Elements;
+
+                foreach(Element element in  elementSet)
+                {
+                    var elementName = element.Name;
+                    elementNames.Add(elementName);
+                    var spaceNumber = element.get_Parameter(BuiltInParameter.SPACE_ASSOC_ROOM_NAME);
+                    spaceNumbers.Add(spaceNumber.AsString());
+                }
+                
+                sb.AppendLine($"System ID: {circuit.CircuitNumber}, Load Class: {loadClass}, Element Description: {string.Join(", ", spaceNumbers)}");
+            }
+            TaskDialog.Show("Electrical System Info", sb.ToString());
+            return Result.Succeeded;
+        }
+                
+    }
+}
+    
+
